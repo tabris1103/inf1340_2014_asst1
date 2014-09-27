2bd6f21a3454445105931ffb78158e0d86a4fbfe
ohjchristine <christine.jihae.oh@gmail.com>
2014-09-27 13:02:48 -0400

    Wrote docstring for assignment 3

--- exercise3.py
+++ exercise3.py
@@ -1,11 +1,33 @@
 #!/usr/bin/env python3
 
-#Christine demo commit
+""" Assignment 1, Exercise 3, INF1340, Fall, 2014. Rock, Paper, Scissors.
+
+This module contains a function called decide_rps.
+
+Example:
+    $ python exercise3.py
+
+"""
 
 def decide_rps(player1, player2):
+    """
+    Returns the results of the Rock, Paper, Scissors game.
 
-    #Crerate a Dictionary element for rock paper scissors passed parameter
-    #Check whether a dictionary element generated from the passed parameter is the member of definition
+    :param:
+        player1, player2 (string): choices made by player1 and player2
+            Accepted values are "R" for Rock, "P" for Paper, and "S" for Scissors
+
+    :return:
+        int: The results of the game
+            If player 1 wins, return 1.
+            If player 2 wins, return 2.
+            If it is a tie game, return 0.
+
+    :raises:
+        TypeError if input is not a string
+        ValueError if input is is not "R", "P", or "S".
+    """
+
 
     return 1
 
