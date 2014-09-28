39765e23b2ebbfad06016514ce6065f160e35ab3
Kyungho Jung <kyungho.jung@mail.utoronto.ca>
2014-09-27 21:18:14 -0400

    Updated the test cases with comments.

--- test_exercise3.py
+++ test_exercise3.py
@@ -14,10 +14,9 @@ __status__ = "Prototype"
 import pytest
 from exercise3 import decide_rps
 
-# Test valid inputs
-
-
-def test_valid_inputs():
+# Test procedures to check whether decide_rps function correctly or not
+# when the valid input parameters have been passed.
+def test_decide_rps():
     """
     Inputs that are the correct format and length
     """
@@ -25,18 +24,17 @@ def test_valid_inputs():
     assert decide_rps("S", "S") == 0
     assert decide_rps("R", "S") == 1
 
-# Test invalid inputs
-
-
-def test_invalid_inputs():
+# Test procedures to check whether decide_rps function handles the exception properly or not
+# when the invalid inputs (in terms of format or value) have been passed.
+def test_inputs():
     """
     Inputs that are incorrect format
     """
     with pytest.raises(TypeError):
-        decide_rps("1", "2")
+        decide_rps(1, "2")
     with pytest.raises(ValueError):
         decide_rps("Rock", "Paper")
-
+        decide_rps("1", "2")
 
 # other tests
 
