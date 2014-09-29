dd46b13108d23a848da693990013c8c2def39876
Kyungho Jung <kyungho.jung@mail.utoronto.ca>
2014-09-29 18:18:21 -0400

    Improved the test for Exercise 3 with more cases

--- test_exercise3.py
+++ test_exercise3.py
@@ -20,9 +20,21 @@ def test_decide_rps():
     """
     Inputs that are the correct format and length
     """
-    assert decide_rps("R", "P") == 2
-    assert decide_rps("S", "S") == 0
+
+    # Case - Player 1 Wins
+    assert decide_rps("P", "R") == 1
     assert decide_rps("R", "S") == 1
+    assert decide_rps("S", "P") == 1
+
+    # Case - Player 2 Wins
+    assert decide_rps("R", "P") == 2
+    assert decide_rps("S", "R") == 2
+    assert decide_rps("P", "S") == 2
+
+    # Case - Ties
+    assert decide_rps("R","R") == 0
+    assert decide_rps("S","S") == 0
+    assert decide_rps("P","P") == 0
 
 # Test procedures to check whether decide_rps function handles the exception properly or not
 # when the invalid inputs (in terms of format or value) have been passed.
@@ -31,9 +43,14 @@ def test_inputs():
     Inputs that are incorrect format
     """
     with pytest.raises(TypeError):
-        decide_rps(1, "2")
+        decide_rps(1, "R")
+        decide_rps("R", 1)
+        decide_rps(2,1)
+        decide_rps(P, R)
+
     with pytest.raises(ValueError):
-        decide_rps("Rock", "Paper")
+        decide_rps("Rock", "P")
+        decide_rps("R", "Paper")
         decide_rps("1", "2")
 
 # other tests
