563743f639aa1ab9289d90ca7da208e5ff36783c
ohjchristine <christine.jihae.oh@gmail.com>
2014-09-30 21:25:47 -0400

    Edits to comments to make more consistent with wording used in assignment sheet

--- exercise2.py
+++ exercise2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-    Perform a checksum on a UPC
+    Verifies that the digits of a UPC code are correct by performing a checksum
     Assignment 1, Exercise 2, INF1340 Fall 2014
 """
 
@@ -16,36 +16,36 @@ __status__ = "Prototype"
 # imports one per line
 
 
-def checksum (upc):
+def checksum(upc):
     """
-    Checks if the digits in a UPC is consistent with checksum
+    Verifies that the digits of a UPC are correct by performing checksum
 
-    :param upc: a 12-digit universal product code
+    :param upc: a 12-digit Universal Product Code
     :return:
         Boolean: True, checksum is correct
         False, otherwise
     :raises:
-        TypeError if input is not a strong
+        TypeError if input is not a string
         ValueError if string is the wrong length (with error string stating how many digits are over or under
     """
 
     is_correct_upc_code = False
 
-    # check type of input
+    # Verify that the type of input is valid
     if type(upc) is str:
-        # check length of string
+        # Verify that the length of string is valid
         if len(upc) != 12:
-            raise ValueError("Length of the passed parameter is not equal to 12")
+            raise ValueError("Length of the UPC input is not equal to 12")
 
-        # checking whether UPC code contains any character or string in it.
+        # Verify that the UPC input does not contain any characters or strings
         elif str(upc).isnumeric() is False:
-            raise ValueError("UPC code must not contain any string/character value")
+            raise ValueError("UPC code must not contain any string/character values")
         else:
-            # convert string to array
+            # Convert string to array
             upc_array = list(upc)
             check_sum = 0
 
-            # generate checksum using the first 11 digits provided
+            # Generate checksum using the first 11 digits of the UPC input
             for string_index in range(0, 11):
                 if string_index % 2 == 0:
                     check_sum += int(upc_array[string_index]) * 3
@@ -54,19 +54,19 @@ def checksum (upc):
                     check_sum += int(upc_array[string_index])
                     print("Check Sum at " + str(string_index) + " = " + str(check_sum))
 
-            #Calculate modulo 10 of check_sum
+            # Calculate modulo 10 of check_sum
             mod_10_of_check_sum = check_sum % 10
 
-            #subtract mod_10_of_check_sum from 10 to get the calculated last digit
+            # Subtract mod_10_of_check_sum from 10 to get the supposed last digit
             subtracting_from_10 = 10 - mod_10_of_check_sum
 
-            # check calculated last digit against the the twelfth digit
-            # return True if they are equal, False otherwise
+            # Compare the supposed last digit with the the actual twelfth digit
+            # Return True if they are equal, False otherwise
             last_digit = int(upc_array[11])
             if subtracting_from_10 == last_digit:
                 is_correct_upc_code = True
     else:
         # raise TypeError if not string
-        raise TypeError("Passed parameter is not string type")
+        raise TypeError("The UPC input is not string type")
 
     return is_correct_upc_code
