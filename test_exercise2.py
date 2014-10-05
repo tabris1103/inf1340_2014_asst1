5c8735817896e20edb50a010331ca10acad160e0
ohjchristine <christine.jihae.oh@gmail.com>
2014-10-04 20:55:34 -0400

    Clean up comments, add test case

--- test_exercise2.py
+++ test_exercise2.py
@@ -2,8 +2,13 @@
 
 """ Module to test exercise2.py """
 
-__author__ = 'Kyungho Jung & Christine Oh'
-__status__ = "Completed"
+__author__ = 'Susan Sim'
+__email__ = "ses@drsusansim.org"
+
+__copyright__ = "2014 Susan Sim"
+__license__ = "MIT License"
+
+__status__ = "Prototype"
 
 # imports one per line
 import pytest
